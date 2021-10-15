29d088a667f29e0277b2453429df98c3146b580d
Naveen Kulkarni <naveenkulkarni029@gmail.com>
2021-10-15 21:45:58 +0530

    backend

---
+++ backend/wallets/src/main/java/com/kn/wallets/WalletsApplication.java
@@ -0,0 +1,13 @@
+package com.kn.wallets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsApplication.class, args);
+	}
+
+}
