da540d471ea6a4f9f419a0d8757f88668553e66a
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2021-10-16 12:58:54 +0530

    Update Wallet.java

--- backend/wallets/src/main/java/com/kn/wallets/domain/Wallet.java
+++ backend/wallets/src/main/java/com/kn/wallets/domain/Wallet.java
@@ -20,7 +20,7 @@ public class Wallet implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "wallet_id")
 	private Long walletId;
 	
